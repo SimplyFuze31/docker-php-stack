9f50a164759667fece618e0adeecea38937ab726
SimplyFuze31 <kondratuiksasha5@gmail.com>
2023-03-30 12:18:13 +0300

    php configuration

--- src/index.php
+++ src/index.php
@@ -1,19 +1,3 @@
 <?php
 declare(strict_types = 1);
-//phpinfo();
-
-
-try {
-    $dbh = new PDO('mysql:host=mysql;dbname=Sandbox', 'root', 'secret');
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
-
-$sql = "INSERT INTO Students (firstname, lastname, email)
-VALUES ('Oleksandr', 'Kondratiuk', 'www@gmail.com');";
-
-
-$result = $dbh->exec("SELECT * FROM Students;");
-
-var_dump($result);
+phpinfo();
